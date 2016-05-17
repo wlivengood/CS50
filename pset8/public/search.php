31bfb86b0f06e551ae8c9f24ce570ea666a96f71
wlivengo@gmail.com <wlivengo@gmail.com>
2016-05-17 15:10:52 -0400

    adding files

---
+++ pset8/public/search.php
@@ -0,0 +1,16 @@
+<?php
+
+    require(__DIR__ . "/../includes/config.php");
+
+    // numerically indexed array of places
+    $places = [];
+
+    // search database for places matching $_GET["geo"], store in $places
+    
+    $places = CS50::query("SELECT * FROM places WHERE MATCH(location) AGAINST (?)", $_GET["geo"]);
+    
+    // output places as JSON (pretty-printed for debugging convenience)
+    header("Content-type: application/json");
+    print(json_encode($places, JSON_PRETTY_PRINT));
+
+?>
